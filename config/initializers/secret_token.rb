82591d76e0777f0b12bf76bb8c21e21304175bec
davidhurley <dhurls99@gmail.com>
2013-12-23 16:02:37 -0500

    what what

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lilly::Application.config.secret_key_base = 'c9bc9087776e4fe3451475f2d64844a781f5e4ee0da01b038fdae049a4cb00b6efecceaffd9f7bd474c9b0787ef358cdbf4fb7ccee90bf6948fe6d3a641c3bb9'
