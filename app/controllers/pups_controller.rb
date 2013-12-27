5921916d42191fffa536601e6a76279b738fcc55
davidhurley <dhurls99@gmail.com>
2013-12-27 18:14:45 -0500

    quick change

--- app/controllers/pups_controller.rb
+++ app/controllers/pups_controller.rb
@@ -1,54 +1,40 @@
 class PupsController < ApplicationController
   before_action :set_pup, only: [:show, :edit, :update, :destroy]
 
-  # GET /pups
-  # GET /pups.json
   def index
-    @pups = Pup.to_a
+    @pups = Pup.all
   end
 
-  # GET /pups/1
-  # GET /pups/1.json
   def show
   end
 
-  # GET /pups/new
   def new
     @pup = current_user.pups.build
   end
 
-  # GET /pups/1/edit
   def edit
   end
 
-  # POST /pups
-  # POST /pups.json
   def create
-    @pup = Pup.current_user.pups.build(pup_params)
-
-      if @pup.save
-         redirect_to @pup, notice: 'Pup was successfully created.' 
-      else
-        render action: 'new' 
+    @pup = current_user.pups.build(pup_params)
+    if @pup.save
+      redirect_to @pup, notice: 'Pup was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /pups/1
-  # PATCH/PUT /pups/1.json
   def update
-   
-      if @pup.update(pup_params)
-         redirect_to @pup, notice: 'Pup was successfully updated.' 
-      else
-        render action: 'edit'   
+    if @pup.update(pup_params)
+      redirect_to @pup, notice: 'Pup was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /pups/1
-  # DELETE /pups/1.json
   def destroy
     @pup.destroy
-     redirect_to pups_url 
+    redirect_to pups_url
   end
 
   private
@@ -64,6 +50,6 @@ class PupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pup_params
-      params.require(:pup).permit(:description, :date, :image)
+      params.require(:pup).permit(:description, :image, :date)
     end
 end
